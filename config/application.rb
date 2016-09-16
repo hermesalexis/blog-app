3a3a7ec96ad5d7685abbcc6d454276267b06972d
hermesalexis <hermesagm@gmail.com>
2016-09-16 13:18:35 -0500

    Se instala la gema i18n y se crear archiv es.ylm

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module BlogApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    I18n.available_locales = [:en, :es]
+    config.time_zone = 'Bogota' # set default time zone to "Bogota" (UTC +4)
+    config.i18n.default_locale = :es# set default locale to Bogota
   end
 end
