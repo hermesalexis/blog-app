33c85ec3b6ccb9ae9d13ce3788b1da3c17c4754d
hermesalexis <hermesagm@gmail.com>
2016-09-17 16:14:09 -0500

    Se crea la vista edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
       @post = Post.new(post_params)
       @post.user = current_user #relaciona usuario actual
       if @post.save 
-      	 redirect_to posts_path
+      	 redirect_to posts_path, notice:"El post fue públicado correctamente"
       else
          render :new
       end
@@ -20,6 +20,19 @@ class PostsController < ApplicationController
 
     def show
      @post = Post.find(params[:id])
+    end
+
+    def edit
+     @post = Post.find(params[:id])
+    end
+
+    def update
+     @post = Post.find(params[:id])
+     if @post.update(post_params)
+     	redirect_to posts_path, notice:"El post se ha modificado correctamente"
+     else
+     	render :edit
+     end
     end
 
     private
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,6 @@
 module PostsHelper
+	def form_title
+	  @post.new_record? ? "Nuevo Post" : "Editar Post"
+		
+	end
 end
