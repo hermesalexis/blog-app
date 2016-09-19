28f086af4b4e7c029210abcec1856c9ee46b275b
hermesalexis <hermesagm@gmail.com>
2016-09-19 14:50:24 -0500

    se agrega controlador accion create para comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+  def create
+	product = Product.find(params[:id])
+	product.comment.create(comments_params)
+	redirect_to post
+  end
+
+  private
+   def comments_params
+   	params.require(:comment).permit(:commentary).merge(user:current_user)
+   end
+
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160915221530) do
+ActiveRecord::Schema.define(version: 20160919194215) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "commentary"
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
